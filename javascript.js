3e322f85b7f104a20e56fccfeb1bd1052bf9cf50
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-03-11 10:17:34 -0400

    first functionality of binary tree building added

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 class Node {
   constructor(data) {
     this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
@@ -9,5 +11,57 @@ class Tree {
     this.root = root;
   }
 
-  buildTree(array) {}
+  sortArray(array) {
+    array.sort((a, b) => a - b);
+    return array;
+  }
+
+  buildTree(array) {
+    let sortedArray = this.sortArray(array);
+    let start = 0;
+    let end = sortedArray.length - 1;
+    return this.buildTreeRecursive(sortedArray, start, end);
+  }
+
+  buildTreeRecursive(array, start, end) {
+    console.log(array);
+    console.log(start);
+    console.log(end);
+    if (start > end) {
+      return null;
+    }
+
+    let mid = start + Math.floor((end - start) / 2);
+
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTreeRecursive(array, start, mid - 1);
+
+    root.right = this.buildTreeRecursive(array, mid + 1, end);
+
+    return root;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+  insert(value) {}
 }
+
+const newTree = new Tree();
+let root = newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+newTree.prettyPrint(root);
